c3914b6da6ae4a6f0121f4fd7371246e54522c05
Thomas Willingham <thomas@tshows.us>
2016-11-03 11:04:41 -0500

    type

---
+++ type.c
@@ -0,0 +1,28 @@
+#include "type.h"
+
+char *STRING = "STRING";
+char *INT = "INT";
+
+char *MINUS = "MINUS";
+char *PLUS = "PLUS";
+char *DIVIDE = "DIVIDE";
+char *MULTIPLY = "MULTIPLY";
+char *NOT = "NOT";
+char *GT = "GT";
+char *LT = "LT";
+char *GTE = "GTE";
+char *LTE = "LTE";
+char *ISEQUAL = "ISEQUAL";
+char *AND = "AND";
+char *OR = "OR";
+char *EQUALS = "EQUALS";
+
+char *OP = "OP";
+char *CP = "CP";
+char *OSB = "OSB";
+char *CSB = "CSB";
+char *OCB = "OCB";
+char *CCB = "CCB";
+char *SEMI = "SEMI";
+char *COMMA = "COMMA";
+char *ENDOFFILE = "ENDOFFILE";
