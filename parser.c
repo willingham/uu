d7832895a4c55acb1fe349387067d0296e3a7c56
Thomas Willingham <thomas@tshows.us>
2016-11-11 20:26:59 -0600

    rec

--- parser.c
+++ parser.c
@@ -1,3 +1,4 @@
+#include "error.h"
 #include "parser.h"
 #include "lexeme.h"
 #include "lexer.h"
@@ -16,3 +17,92 @@ Lexeme *parse(FILE *xfile) {
     }
     return p->pending;
 }
+
+// lhs grammar functions
+void program(void) {
+    expressionList();
+}
+
+void expressionList(void) {
+    expression();
+    if (expressionListPending()) {
+        expressionList();
+    }
+}
+
+void expression(void) {
+    if(exprPending()) {
+        expr();
+        match(SEMI);
+    } else if (loopPending()) {
+        loop();
+    } else if (iffPending()) {
+        iff();
+    } else if (funcDefPending()) {
+        funcDef();
+    } else {
+        error("Expression not found.\n");
+        exit(1);
+    }
+}
+
+void expr(void) {
+    primary();
+    if(operatorPending()) {
+        operator();
+        expr();
+    }
+}
+
+void optParamList(void) {
+    if(paramListPending()) {
+        paramList();
+    }
+}
+void paramList(void) {
+    expr();
+    if(exprPending()) {
+        match(COMMA);
+        paramList();
+    }
+}
+
+void primary(void) {
+    if(literalPending()) {
+        literal();
+    } else if(check(OP)) {
+        match(OP);
+        expr();
+        match(cp);
+    } else if(lambdaPending()) {
+        lembda();
+    } else if(check(ID)) {
+        match(ID);
+        if(check(OSB)) {
+            match(OSB);
+            literal();
+            match(CSB);
+        } else if(check(OP)) {
+            match(OP);
+            optParamList();
+            match(CP);
+        }
+    } else if(operatorPending()) {
+        operator();
+        primary();
+    } else {
+        error("Primary not found.\n");
+        exit(1);
+    }
+}
+
+extern void operator(void);
+extern void literal(void);
+extern void funcDef(void);
+extern void lambda(void);
+extern void loop(void);
+extern void block(void);
+extern void whilee(void);
+extern void forr(void);
+extern void iff(void);
+extern void optElse(void);
--- parser.h
+++ parser.h
@@ -16,4 +16,27 @@ typedef struct parser {
 
 extern Lexeme *parse(FILE *fIn);
 
+extern int check(char *x);
+extern void match(char *x);
+
+// lhs grammar functions
+extern void program(void);
+extern void expressionList(void);
+extern void expression(void);
+extern void expr(void);
+extern void optParamList(void);
+extern void paramList(void);
+extern void primary(void);
+extern void operator(void);
+extern void literal(void);
+extern void funcDef(void);
+extern void lambda(void);
+extern void loop(void);
+extern void block(void);
+extern void whilee(void);
+extern void forr(void);
+extern void iff(void);
+extern void optElse(void);
+
+
 #endif
