edfdb1375807e8db5c31dd7bd0f1b51118f76959
Thomas Willingham <twillingham@crimson.ua.edu>
2016-11-20 13:26:04 -0600

    env

--- env.c
+++ env.c
@@ -13,3 +13,33 @@ Lexeme *extendEnv(Lexeme *env, Lexeme *vars, Lexeme *vals) {
 Lexeme *makeTable(Lexeme *vars, Lexeme *vals) {
     return cons(TABLE, vars, vals);
 }
+
+Lexeme *lookupEnv(Lexeme *env, Lexeme *var) {
+    while (env != NULL) {
+        Lexeme *table = car(env);
+        Lexeme *vars = car(table);
+        Lexeme *vals = cdr(table);
+        while (vars != NULL) {
+            if (sameVariable(var, car(vars))) {
+                return car(vals);
+            }
+            //walk the lists in parallel
+            vars = cdr(vars);
+            vals = cdr(vals);
+        }
+        env = cdr(env);
+    }
+    fprintf(stderr, "FATAL: variable, %s, is undefined.", var->sval);
+    return NULL;
+}
+
+int sameVariable(Lexeme *x, Lexeme *y) {
+    return !strcmp(x->sval, y->sval);
+}
+
+Lexeme *insert(Lexeme *var, Lexeme *val, Lexeme *env) {
+    var table = car(env);
+    setCar(table, cons(GLUE, var, car(table)));
+    setCdr(table, cons(GLUE, val, cdr(table)));
+    return val;
+}
